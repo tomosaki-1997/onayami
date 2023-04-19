a57070c1ddaa5500a197aeb699b01b3833771c68
tk-katu <109959438+tomosaki-1997@users.noreply.github.com>
2023-04-19 22:05:55 +0900

    Update index.blade.php

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -15,34 +15,14 @@
 
     <div class="container">
         <div class="new">
-            <h1>最新の投稿</h1>
-            <div class="contents-list">
-                <ul>
-                    @foreach($contents as $content)
-                    <li>
-                        <div class="contents">
-                            <a class="link" href="/detail/{{ $content->id}}">
-                                <div class="contents-detail">
-                                    <div class="contents-image">
-                                        <object><a href="/userpage/{{$content->user->id}}">
-                                            <img src="{{asset('storage/images/'.$content->user->image)}}" alt="">
-                                        </a></object>
-                                    </div>
-                                    <div class="contents-title">
-                                        <h2>{{ $content->title }}</h2>
-                                        <h3>{{ $content->user->name }}</h3>
-                                        <div class="good-count">
-                                            <h4>♥</h4><p>{{ $content->good()->count()}}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </li>
-                    @endforeach
-                </ul>
+            <div class="flex" style="margin-top: 5rem;">
+                <form action="{{ route('index') }}" method="GET"  style="text-align: right;">
+                    <input type="text"  name="keyword" placeholder="キーワードを入れる" value="{{ $keyword }}">
+                    <input type="submit" value="検索する">
+
+                </form>
             </div>
-            {{ $contents->links('pagination::bootstrap-4') }}
+
         </div>
         <div class="new">
             <h1>人気の投稿</h1>
@@ -55,14 +35,24 @@
                                 <div class="contents-detail">
                                     <div class="contents-image">
                                         <object><a href="/userpage/{{$content->user->id}}">
+                                            @if($content->user->image)
                                             <img src="{{asset('storage/images/'.$content->user->image)}}" alt="">
+                                            @else
+                                            <img src="{{asset('storage/images/default.png')}}" alt="">
+                                            @endif
                                         </a></object>
                                     </div>
                                     <div class="contents-title">
                                         <h2>{{ $content->title }}</h2>
                                         <h3>{{ $content->user->name }}</h3>
+                                          <div>{!! Str::limit($content->content, 50, '...続きを読む') !!}</div>
                                         <div class="good-count">
-                                            <h4>♥</h4><p>{{ $content->good()->count()}}</p>
+                                            <h4>♥{{ $content->good()->count()}}</h4>
+                                            <div class="card-footer">
+                                                <span class="mr-2 float-right">
+                                                     投稿日時 {{$content->created_at->diffForHumans()}}
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -71,9 +61,55 @@
                     </li>
                     @endforeach
                 </ul>
-                
+
             </div>
         </div>
+
+        <div class="new">
+            <h1>投稿一覧（最新版）</h1>
+            <div class="contents-list">
+                <ul>
+                    @foreach($contents as $content)
+                    <li>
+                        <div class="contents">
+                            <a class="link" href="/detail/{{ $content->id}}">
+                                <div class="contents-detail">
+                                    <div class="contents-image">
+                                        <object><a href="/userpage/{{$content->user->id}}">
+                                            @if($content->user->image)
+                                            <img src="{{asset('storage/images/'.$content->user->image)}}" alt="">
+                                            @else
+                                            <img src="{{asset('storage/images/default.png')}}" alt="">
+                                            @endif
+                                        </a></object>
+                                    </div>
+                                    <div class="contents-title">
+                                        <h2>{{ $content->title }}</h2>
+                                        <h3>{{ $content->user->name }}</h3>
+                                        {{-- ここを追加 --}}
+                                        <div>{!! Str::limit($content->content, 50, '...続きを読む') !!}</div>
+                                        {{-- ここを追加 --}}
+
+                                        <div class="good-count">
+                                            <h4>♥{{ $content->good()->count()}}</h4>
+                                            <div class="card-footer">
+                                               <span class="mr-2 float-right">
+                                                   投稿日時 {{$content->created_at->diffForHumans()}}
+                                               </span>
+
+                                           </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
     </div>
     @include('includes.footer')
 </body>
+</html>

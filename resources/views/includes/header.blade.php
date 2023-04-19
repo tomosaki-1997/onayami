834d2923de0f62a12dc83a5327abc737049dc73c
tk-katu <109959438+tomosaki-1997@users.noreply.github.com>
2023-04-19 22:28:17 +0900

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="header-banner">
         <div class="header-nav">
-            <a href="/"><h1>DTMagnet</h1></a>
+            <a href="/"><h1>お悩み掲示板</h1></a>
             {{-- <div class="search">
                 <input type="text">
                 検索
@@ -9,15 +9,18 @@
             <div class="header-right">
                 <ul>
                     @auth
-                    <li ><a class="mypage" href="/mypage">{{ Auth::user()->name}}</a></li>
-                    <li><a href="/create">投稿</a></li>
-                    <li><a href="/random">ランダム</a></li>
+                    <li ><a class="mypage" href="/mypage">マイページ</a></li>
+                    @if(Auth::user()->role == 0)
+                    <li><a href="/users">ユーザーリスト</a></li>
+                    @endif
+                    <li><a href="/create">新規投稿</a></li>
+
                     <!-- <li><a href="/">投稿一覧</a></li> -->
                     <li><a href="/setting">設定</a></li>
                     <li>
                         <form method="POST" name="form1" action="{{route('logout')}}">
                             @csrf
-                            <input type="hidden"> 
+                            <input type="hidden">
                             <a href="javascript:form1.submit()">ログアウト</a>
                         </form>
                     </li>
